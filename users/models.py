39816c838760f10a8c6ff5b2357a1909cc608fa4
Evans <snaveqiptoo@gmail.com>
2024-12-08 03:58:18 +0300

    Telemedicine Users App

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager, PermissionsMixin
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        # Ensure that the superuser is created with the required fields
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser , PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # You can add fields that are required when creating a superuser
+
+    def __str__(self):
+        return self.email
