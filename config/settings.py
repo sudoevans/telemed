39b412ccba1c9b77d1623d02df8a95e875fcde71
Evans <snaveqiptoo@gmail.com>
2024-12-10 17:55:34 +0300

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from datetime import timedelta
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,13 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Personal apps
+    # Own apps
     'users',
+    'patients',
+    'doctors',
+    'appointments',
+    'notifications',
 
     # Third party
     'rest_framework',
     'rest_framework.authtoken',
-    'djoser'
+    'djoser',
+    'phonenumber_field' #lol i cant write Regex, stick with this one
 
 ]
 
@@ -153,3 +160,15 @@ DJOSER = {
         'user': 'users.serializers.UserSerializer',
     },
 }
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "TOKEN_OBTAIN_SERIALIZER": "users.serializers.LoginSerializer",
+}
+
+
+AFRICASTALKING_USERNAME = os.getenv('AFRICASTALKING_USERNAME')
+AFRICASTALKING_API_KEY = os.getenv('AFRICASTALKING_API_KEY')
